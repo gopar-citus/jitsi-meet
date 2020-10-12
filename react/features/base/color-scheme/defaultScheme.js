9e1cf50e28a5f7860bdfe986dc32e2486137bbe9
mskim <mskim@citus.co.kr>
2020-10-12 17:09:21 +0900

    나가기 버튼 바탕색 변경.

--- react/features/base/color-scheme/defaultScheme.js
+++ react/features/base/color-scheme/defaultScheme.js
@@ -55,6 +55,7 @@ export default {
         button: 'rgb(255, 255, 255)',
         buttonToggled: 'rgb(38, 58, 76)',
         buttonToggledBorder: getRGBAFormat('#a4b8d1', 0.6),
-        hangup: 'rgb(225, 45, 45)'
+        //hangup: 'rgb(225, 45, 45)'
+        hangup: 'rgb(233, 69, 72)'
     }
 };
